76400e9f544b9d8e9e6e55e39f60dbac245ebef6
Hobson Lane <shopper@totalgood.com>
2014-05-10 13:51:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo apt-get install git python python-yaml python-jinja2 aptitude
+sudo yum install git python python-yaml python-jinja2 
+
 cd ~
-mkdir workspace
+mkdir ansible_workspace
 cd workspace
 git clone git@github.com:hobson/dotfiles.git
